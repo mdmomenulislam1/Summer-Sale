d18d8e21e1835136f730cce3de5886c364b0d04e
mdmomenulislam1 <mdmomenulislam1@gmail.com>
2023-08-20 19:33:29 +0600

    Final commit

--- index.js
+++ index.js
@@ -1,45 +1,52 @@
-
-
 let total = 0;
 let discount = 0;
+let dam = 0;
+const paymentBtn = document.getElementById('payment-btn');
+const couponElement = document.getElementById('coupon-area').value;
+const applyCoupon = document.getElementById('coupon-btn');
+const productName = document.getElementById('item-name');
+const reSetBtn = document.getElementById('reset-btn');
+
+
 
 function clickPriceItem(tar) {
-    const productName = document.getElementById('item-name');
-    const itemName = tar.parentNode.childNodes[3].innerText;
-    const li = document.createElement('li');
-    li.innerText = itemName;
-    productName.appendChild(li);
-
-    const damText = tar.parentNode.childNodes[5].innerText.split(" ")[0];
-    total = parseFloat(total) + parseFloat(damText);
-
+    const itemName = tar.children[1].children[1].innerText;
+    const count = productName.childElementCount;
+    dam = tar.children[1].children[2].innerText.split(" ")[0];
+    total = parseFloat(total) + parseFloat(dam);
     const totalPrice = document.getElementById('total-price').innerText = total.toFixed(2);
-
-    const coupon = document.getElementById('coupon-btn');
-
+    const li = document.createElement('li');
+    li.innerHTML = `${count + 1}. ${itemName} <span>${dam} $</span>`;
+    productName.appendChild(li);
     if (total < '200') {
-        coupon.disabled = true;
+        applyCoupon.disabled = true;
     } else {
-        coupon.disabled = false;
-        const discountText = (20 * total) / 100;
-        discount = document.getElementById('discount').innerText = discountText.toFixed(2);
+        applyCoupon.disabled = false;
     }
-
-
-    const paymentBtn = document.getElementById('payment-btn');
-
+    applyCoupon.addEventListener('click', function () {
+        const couponElement = document.getElementById('coupon-area').value;
+        if (couponElement === 'SELL200') {
+            const discountText = (20 * total) / 100;
+            discount = document.getElementById('discount').innerText = discountText.toFixed(2);
+            const netTotal = total - discount;
+            console.log(netTotal);
+            const final = document.getElementById('net-total').innerText = netTotal.toFixed(2);
+        } else{
+            const netTotal = total - discount;
+            console.log(netTotal);
+            const final = document.getElementById('net-total').innerText = netTotal.toFixed(2);
+            return;
+        }
+    })
     if (total < '0' || total === '') {
         paymentBtn.disabled = true;
     } else {
         paymentBtn.disabled = false;
-        const netTotal = total - discount;
-        const final = document.getElementById('net-total').innerText = netTotal.toFixed(2);
     }
-
-    
-}
-
-function goHomeClick(){
-    totalPrice = '';
-    return;
+    reSetBtn.addEventListener('click',function(){
+        productName.innerText = '';
+        document.getElementById('total-price').innerText = '';
+        document.getElementById('discount').innerText = '';
+        document.getElementById('net-total').innerText = '';
+    })
 }
